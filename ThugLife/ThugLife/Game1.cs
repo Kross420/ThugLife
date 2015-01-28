932a84e8414ca631837925e17b46269e6061227d
Kristians <kritianz1@inbox.lv>
2015-01-28 18:24:53 +0200

    Player, game1 un attēli

--- ThugLife/ThugLife/Game1.cs
+++ ThugLife/ThugLife/Game1.cs
@@ -11,59 +11,45 @@ using Microsoft.Xna.Framework.Media;
 
 namespace ThugLife
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        Player player;
+        
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-        ///lel
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+
+        //
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            player = new Player();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        //
         protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
+        { 
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,GraphicsDevice.Viewport.TitleSafeArea.Y +GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+            player.Initialize(Content.Load<Texture2D>("policecar/popo"), playerPosition);
 
-            // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
+        //
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        //
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
@@ -75,15 +61,17 @@ namespace ThugLife
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        //
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin(); // Start drawing
+
+            // Draw the Player
+            player.Draw(spriteBatch);
+
+            spriteBatch.End(); // Stop drawing
 
             base.Draw(gameTime);
         }
---
+++ ThugLife/ThugLife/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ThugLife
+{
+    class Player
+    {
+
+        public Texture2D PlayerImage; //Spēlētāja bilde
+        Vector2 Position; // pozīcija
+        public bool Active; // stāvoklis
+        public int Health; // dzīvības
+        // iegūst bildes izmērus
+        public int Width
+        {
+            get { return PlayerImage.Width; }
+        }
+        public int Height
+        {
+            get { return PlayerImage.Height; }
+        }
+
+
+        //uzstāda sākuma parametrus
+        public void Initialize(Texture2D image, Vector2 position)
+        {
+            PlayerImage = image;
+            Position = position;
+            Active = true;
+            Health = 100;
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(PlayerImage, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
